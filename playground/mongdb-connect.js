33d9eafc62dc73546c6b2ff886c46354c6bb5dbf
Vaishali <vaishali.verma@taistech.com>
2018-04-02 17:12:24 +0530

    Init commit

---
+++ playground/mongdb-connect.js
@@ -0,0 +1,32 @@
+//const MongoClient =  require('mongodb').MongoClient;
+const {MongoClient,ObjectID} =  require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err,db) =>{
+  if(err){
+    return console.log('Unable to connect to MongoDB server');
+  }
+  console.log('Connected to MongoDB server');
+  //const db =client.db('TodoApp');
+  // db.collection('Todos').insertOne({
+  //   text:'Something to do',
+  //   completed:false
+  // },(err,result) =>{
+  //   if(err){
+  //     console.log('Unable to insert todo',err);
+  //   }
+  //   console.log(JSON.stringify(result.ops,undefined,2));
+  // });
+  db.collection('Users').insertOne({
+    name:'Vaishali',
+    age:27,
+    location:'Gurgaon'
+  },(err,result) =>{
+    if(err){
+      console.log('Unable to insert to User collection',err);
+    }
+    console.log(JSON.stringify(result.ops,undefined,2));
+    console.log(result.ops[0]._id.getTimestamp());
+  });
+  //client.close();
+  db.close();
+});
