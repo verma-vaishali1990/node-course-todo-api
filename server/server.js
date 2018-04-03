017dc61f6c3a9a941a632c99ef2aeadc91ab5e06
Vaishali <vaishali.verma@taistech.com>
2018-04-03 23:55:48 +0530

    Update route for TodoApp

--- server/server.js
+++ server/server.js
@@ -1,14 +1,14 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-
+const express = require('express');
+const bodyParser = require('body-parser');
+const _ = require('lodash');
+const {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} =  require('./models/user');
-const {ObjectID} = require('mongodb');
-
 var port = process.env.PORT || 3000;
 var app = express();
+
 app.use(bodyParser.json());
 app.post('/todos',(req,res) =>{
   var todo = new Todo({
@@ -23,6 +23,7 @@ app.post('/todos',(req,res) =>{
   console.log(req.body);
 });
 
+//Get All Todos
 app.get('/todos',(req,res)=>{
   Todo.find().then((todos)=>{
     res.send({todos});
@@ -31,6 +32,7 @@ app.get('/todos',(req,res)=>{
   });
 });
 
+//Add todo
 app.get('/todos/:id',(req,res) =>{
   var id = req.params.id;
   if(!ObjectID.isValid(id)){
@@ -48,6 +50,7 @@ app.get('/todos/:id',(req,res) =>{
   });
 });
 
+//Delete
 app.delete('/todos/:id',(req,res) =>{
   var id =req.params.id;
   if(!ObjectID.isValid){
@@ -62,6 +65,35 @@ app.delete('/todos/:id',(req,res) =>{
     }
   }).catch((error)=>{
       res.status(400).send();
+  });
+});
+
+//Update
+app.patch('/todos/:id',(req,res) =>{
+  var id =req.params.id;
+  var body =_.pick(req.body,['text','completed']);
+
+  if(!ObjectID.isValid){
+   return res.status(404).send();
+  }
+
+  if(_.isBoolean(body.completed) && body.completed){
+    body.completedAt = new Date().getTime();
+  }else{
+    body.completed =false;
+    body.completedAt = null;
+  }
+  Todo.findByIdAndUpdate(id,{
+    $set : body
+  },{
+    new :true
+  }).then((todo) =>{
+    if(!todo){
+      return res.status(404).send();
+    }
+    res.send({todo});
+  }).catch((e) =>{
+    res.status(400).send();
   });
 });
 app.listen(port,() =>{
