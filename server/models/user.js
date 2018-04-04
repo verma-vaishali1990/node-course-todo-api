d76a95a0eef8366b17e20a9fdb4f012a55550183
Vaishali <vaishali.verma@taistech.com>
2018-04-04 22:18:50 +0530

    Logout user

--- server/models/user.js
+++ server/models/user.js
@@ -49,6 +49,17 @@ UserSchema.methods.toJSON = function() {
   return _.pick(userObj,['email','_id']);
 };
 
+UserSchema.methods.removeToken = function(token){
+  var user = this;
+  return user.update({
+    $pull :{
+      tokens:{
+        token
+      }
+    }
+  });
+};
+
 UserSchema.statics.findByToken = function(token){
  var User = this;
  var decoded ;
@@ -63,8 +74,27 @@ try{
     'tokens.token':token,
     'tokens.access':'auth'
   });
+};
 
-}
+UserSchema.statics.findByCredentials = function(email,password){
+  var User = this;
+ return User.findOne({email}).then((user) =>{
+   if(!user){
+     return Promise.reject();
+   }
+   return new Promise((resolve,reject) =>{
+     bcrypt.compare(password,user.password,(error,result) =>{
+       if(result){
+          resolve(user);
+       }
+       else{
+         reject();
+       }
+     });
+   });
+ });
+};
+
 
 UserSchema.pre('save', function(next) {
   var user =this;
--- server/server.js
+++ server/server.js
@@ -113,6 +113,24 @@ app.post('/users',(req,res) =>{
   });
 });
 
+app.post('/users/login',(req,res) =>{
+   var body =_.pick(req.body,['email','password']);
+   User.findByCredentials(body.email,body.password).then((user) =>{
+     user.generateAuthToken().then((token) =>{
+         res.header('x-auth',token).send({user});
+     });
+   }).catch((e) =>{
+      res.status(400).send();
+   });
+});
+
+app.delete('/users/me/logout',authenticate,(req,res) =>{
+  req.user.removeToken(req.token).then(() =>{
+    res.status(200).send();
+  }).catch((e) =>{
+    res.status(400).send();
+  });
+});
 
 app.get('/users/me', authenticate, (req,res) =>{
  res.send(req.user);
